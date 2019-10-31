48e58eafacd6b959a0c930333aa38cf57a48ae97
QueenDekimZ <37538056+QueenDekimZ@users.noreply.github.com>
2019-10-31 20:12:16 +0800

    1.1

--- computeapp/views.py
+++ computeapp/views.py
@@ -16,6 +16,7 @@ def run_code(code):
         code = 'print(' + code + ')'
         output = subprocess.check_output(['python', '-c', code], universal_newlines=True, stderr=subprocess.STDOUT,
                                          timeout=30)
+        # subprocess.check_out(),执行指定的命令，如果执行状态码为0则返回命令执行结果，否则抛出异常。
     except subprocess.CalledProcessError as e:
         output = '公式格式错误'
     return output
